3faaed989f5bb1ae7bb2468a56100fb741f91f8f
kimearwud22 <kimearwud22@gmail.com>
2023-05-24 21:48:07 +0700

    create prisma orm

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,7 +1,16 @@
 import Card from "../../components/admin/dashboard/card";
 import Layout from "../../components/admin/utils/layout";
+import {useEffect} from 'react'
+import {getCookie, validateToken} from '../../libs/cookie.lib'
 
 export default function Index() {
+  useEffect(() => {
+    if(getCookie('token') === "" && getCookie("username") === ""){
+      window.location.href = "/login";
+    }else{
+      validateToken(getCookie('token')) ? null : window.location.href = "/login";
+    }
+  }, [])
   return (
     <>
       <Layout>
--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,46 @@
+import {useState} from 'react';
+import {useRouter} from 'next/router';
+import {setCookie} from '../libs/cookie.lib.js'
+import Swl from 'sweetalert2';
+
 export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState("")
+  const router = useRouter();
+
+  const handleLogin = (e) =>{
+    e.preventDefault();
+    setLoading(true);
+    fetch('api/auth/login', {
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username,
+        password
+      }),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      if(data.message === "Login success" && data.user.token){
+        Swl.fire("Login Success!", "Login Success Redirected in 3 second!", "success");
+        setCookie("token", data.user.token, 1);
+        router.push("/admin");
+      } else {
+        setError(data.message);
+      }
+      setLoading(false);
+    })
+    .catch((err) => {
+      setError(err);
+      Swl.fire("Login Failed!", "Login Failed Redirected in 3 secon!" + err, "error");
+      setLoading(false)
+    });
+  };
+
   return (
     <section className="login_box_area section-margin">
       <div className="container">
@@ -6,7 +48,7 @@ export default function Login() {
           <div className="col-lg-12">
             <div className="login_form_inner">
               <h3>Login to admin</h3>
-              <form className="row login_form" action="#/" id="contactForm">
+              <form className="row login_form" action="#/" id="contactForm"  >
                 <div className="col-md-12 form-group">
                   <input
                     type="text"
@@ -16,6 +58,9 @@ export default function Login() {
                     placeholder="Username"
                     onfocus="this.placeholder = ''"
                     onblur="this.placeholder = 'Username'"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+
                   />
                 </div>
                 <div className="col-md-12 form-group">
@@ -27,6 +72,8 @@ export default function Login() {
                     placeholder="Password"
                     onfocus="this.placeholder = ''"
                     onblur="this.placeholder = 'Password'"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                   />
                 </div>
                 <div className="col-md-12 form-group">
@@ -34,6 +81,7 @@ export default function Login() {
                     type="submit"
                     value="submit"
                     className="button button-login w-100"
+                    onClick={handleLogin}
                   >
                     Log In
                   </button>
